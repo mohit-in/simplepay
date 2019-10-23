95e7e8cdd133cd604a454dc21f5c692d688898f4
Mohit Sharma <ermohit2018@gmail.com>
2019-10-23 16:36:02 +0530

    unit test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,13 +2,44 @@
 
 namespace App\Tests\Unit;
 
-use App\Repository\UserRepository;
-use PHPUnit\Framework\TestCase;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class UserTest extends TestCase
+
+class UserTest extends KernelTestCase
 {
-	public function testFindByEmail()
-   	{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
 
+
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testFindByEmail()
+    {
+        $user = $this->entityManager
+            ->getRepository(User::class)
+            ->findByEmail("mohit@gmail.com");
+
+        dump($user[0]['name']);exit;
+        $this->assertSame(9999345816, $user['mobile']);
+    }
+
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        // doing this is recommended to avoid memory leaks
+        $this->entityManager->close();
+        $this->entityManager = null;
     }
 }
