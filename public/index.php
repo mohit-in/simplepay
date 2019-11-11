1775acc2f4e4d4883df4f35860df2482267d1b6f
Mohit Sharma <ermohit2018@gmail.com>
2019-11-11 17:10:01 +0530

    jenkins updated file

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
-if (!$_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     Debug::enable();
